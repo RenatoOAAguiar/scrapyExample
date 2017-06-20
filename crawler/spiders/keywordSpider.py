b51fe5bbb07a1ba3c70b317e56aa6bf8f0349650
Renato Aguiar <renatooaa@outlook.com>
2017-06-20 11:54:47 -0300

    Correção

--- crawler/spiders/keywordSpider.py
+++ crawler/spiders/keywordSpider.py
@@ -4,8 +4,6 @@ from crawler.common.searResultPages import searResultPages
 from crawler.common.searchEngines import SearchEngineResultSelectors
 from scrapy.selector import  Selector
 import json
-import re
-import html2text
 
 class keywordSpider(Spider):
     name = 'keywordSpider'
@@ -14,7 +12,6 @@ class keywordSpider(Spider):
     keyword = None
     searchEngine = None
     selector = None
-    cont = 0
 
     def __init__(self, keyword, se = 'bing', pages = 1,  *args, **kwargs):
         super(keywordSpider, self).__init__(*args, **kwargs)
@@ -23,20 +20,16 @@ class keywordSpider(Spider):
         self.selector = SearchEngineResultSelectors[self.searchEngine]
         pageUrls = searResultPages(keyword, se, int(pages))
         for url in pageUrls:
-	    self.cont = self.cont + 1
-	    with open(str(self.cont) + "texto.json", "w") as outfile:
-            	json.dump({'url_body':body}, outfile, indent=4)
             print(url)
             self.start_urls.append(url)
             
 
     def parse(self, response):
-        body = Selector(response).xpath('//body//p//text()').extract()
+	# body = Selector(response).xpath('//body//p//text()').extract()
         ## Dump the output to json file
-	for i in range(self.cont, 0):
-		with open(str(self.cont) + "texto.json", "w") as outfile:
-			feeds.append({'url_body':body})
-			json.dump(feeds, outfile, indent=4)
+	# for i in range(self.cont, 0):
+	#	with open("texto.json", "w") as outfile:
+	#		json.dump({'url_body':body}, outfile, indent=4)
         for url in Selector(response).xpath(self.selector).extract():
             yield {'url':url}
 
