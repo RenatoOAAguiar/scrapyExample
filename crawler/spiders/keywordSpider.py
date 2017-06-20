bb619ba5b0b88c96a7c07f7e895251dd132cfab3
Renato Aguiar <renatooaa@outlook.com>
2017-06-20 10:03:52 -0300

    Alterações.

--- crawler/spiders/keywordSpider.py
+++ crawler/spiders/keywordSpider.py
@@ -11,7 +11,7 @@ class keywordSpider(Spider):
     keyword = None
     searchEngine = None
     selector = None
-
+    cont = 0
 
     def __init__(self, keyword, se = 'bing', pages = 1,  *args, **kwargs):
         super(keywordSpider, self).__init__(*args, **kwargs)
@@ -25,6 +25,10 @@ class keywordSpider(Spider):
 
     def parse(self, response):
         for url in Selector(response).xpath(self.selector).extract():
+            response.xpath('//body//p//text()').extract()
+            self.cont = self.cont + 1
+            file = open(str(self.cont) + '.txt', 'wb')
+            file.write(response.xpath('//body//p//text()').extract())
             yield {'url':url}
 
         pass
