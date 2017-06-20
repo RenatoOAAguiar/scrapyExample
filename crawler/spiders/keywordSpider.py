3509d34fd9c654fd3d1d27f9784afcc04cf6e9fc
Renato Aguiar <renatooaa@outlook.com>
2017-06-20 10:18:27 -0300

    Teste.

--- crawler/spiders/keywordSpider.py
+++ crawler/spiders/keywordSpider.py
@@ -3,6 +3,8 @@ from scrapy.spiders import Spider
 from crawler.common.searResultPages import searResultPages
 from crawler.common.searchEngines import SearchEngineResultSelectors
 from scrapy.selector import  Selector
+import json
+import re
 
 class keywordSpider(Spider):
     name = 'keywordSpider'
@@ -23,7 +25,18 @@ class keywordSpider(Spider):
             self.start_urls.append(url)
 
     def parse(self, response):
+        sel = Selector(response)
+        google_search_links_list = sel.xpath('//h3/a/@href').extract()
+        google_search_links_list = [re.search('q=(.*)&sa',n).group(1) for n in google_search_links_list]
+        
+        ## Dump the output to json file
+        with open("texto", "w") as outfile:
+            json.dump({'output_url':google_search_links_list}, outfile, indent=4)
+        
         for url in Selector(response).xpath(self.selector).extract():
+            #cont = cont + 1
+            #file = open(str(cont) + '.txt', 'wb')
+            #file.write(response.xpath('//body').extract())
             yield {'url':url}
 
         pass
