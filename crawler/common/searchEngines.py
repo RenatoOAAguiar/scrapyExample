8e033956337382131df7c5ea779e0bfeeec41766
Renato Aguiar <renatooaa@outlook.com>
2017-06-20 08:41:21 -0300

    Primeiro commit

---
+++ crawler/common/searchEngines.py
@@ -0,0 +1,12 @@
+__author__ = 'tixie'
+
+SearchEngines = {
+    'google': 'https://www.google.com.br/search?q={0}',
+    'bing': 'http://www.bing.com/search?q={0}&first={1}'
+}
+
+
+SearchEngineResultSelectors= {
+    'google': '//h3/a/@href',
+    'bing':'//h2/a/@href',
+}
---
+++ crawler/spiders/keywordSpider.py
@@ -0,0 +1,30 @@
+__author__ = 'tixie'
+from scrapy.spiders import Spider
+from crawler.common.searResultPages import searResultPages
+from crawler.common.searchEngines import SearchEngineResultSelectors
+from scrapy.selector import  Selector
+
+class keywordSpider(Spider):
+    name = 'keywordSpider'
+    allowed_domains = ['bing.com','google.com']
+    start_urls = []
+    keyword = None
+    searchEngine = None
+    selector = None
+
+
+    def __init__(self, keyword, se = 'bing', pages = 1,  *args, **kwargs):
+        super(keywordSpider, self).__init__(*args, **kwargs)
+        self.keyword = keyword.lower()
+        self.searchEngine = se.lower()
+        self.selector = SearchEngineResultSelectors[self.searchEngine]
+        pageUrls = searResultPages(keyword, se, int(pages))
+        for url in pageUrls:
+            print(url)
+            self.start_urls.append(url)
+
+    def parse(self, response):
+        for url in Selector(response).xpath(self.selector).extract():
+            yield {'url':url}
+
+        pass
