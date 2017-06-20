a7ca8ecb1a5b2672072c74e6ea1e3aa74ff28c1b
Renato Aguiar <renatooaa@outlook.com>
2017-06-20 09:34:48 -0300

    Correção.

--- crawler/pipelines.py
+++ crawler/pipelines.py
@@ -7,15 +7,11 @@
 from scrapy.exceptions import DropItem
 import  logging
 
-cont = 0
-
 class SespiderPipeline(object):
     def __init__(self):
-        self.file = open(str(cont) + '.txt', 'wb')
+        self.file = open('urls.txt', 'wb')
         #self.bloomFilter = rBloomFilter.rBloomFilter(100000, 0.01, 'bing')
 
     def process_item(self, item, spider):
-        cont  = cont + 1
-        for it in item:
-            self.file.write(item['body']+'\n')
+        self.file.write(item['url']+'\n')
         return item
