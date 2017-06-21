0dee0b92a4ddd91cb0f72e743e6ef79bcc06b7cf
Renato Aguiar <renatooaa@outlook.com>
2017-06-21 09:06:13 -0300

    add teste.

---
+++ crawler/spiders/teste.py
@@ -0,0 +1,14 @@
+import scrapy
+
+class BlogSpider(scrapy.Spider):
+    name = 'blogspider'
+    start_urls = ['https://www.google.com.br/?gws_rd=ssl#q=37640083000193']
+
+    def parse(self, response):
+        for title in response.css('body'):
+            yield {
+            'endereco': title.css('body ::text').extract()
+            }
+
+        for next_page in response.css('div > a'):
+            yield response.follow(next_page, self.parse)
