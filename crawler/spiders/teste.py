2e834bf2194a68259188d2fd9fa14724a6696e85
Renato Aguiar <renatooaa@outlook.com>
2017-06-21 09:33:08 -0300

    alteração.

--- crawler/spiders/teste.py
+++ crawler/spiders/teste.py
@@ -10,5 +10,5 @@ class BlogSpider(scrapy.Spider):
             'endereco': title.css('body ::text').extract()
             }
 
-        for next_page in response.css('div > a'):
+        for next_page in response.css('h3.r > a'):
             yield response.follow(next_page, self.parse)
